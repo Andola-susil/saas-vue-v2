71eb9ba54672985526e0215a6137d069ff9fdd6c
Andola_Susil <susil.tripathy@andolasoft.co.in>
2024-06-12 18:33:00 +0530

    Added timesheet table UI

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '192.168.2.34', // or your desired host
+    port: 5174
+  }
 })
