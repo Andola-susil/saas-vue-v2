ddef0e15a38f6efa447b2e2f75420f066720047b
Binapani <andolasoft.user179@gmail.com>
2024-06-13 05:20:21 +0000

    Fix sidebar ui

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    host: '192.168.2.34', // or your desired host
-    port: 5174
-  }
+
 })
