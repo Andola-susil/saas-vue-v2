22c2f81e687ffd0def53a100bb5e79d483072653
Binapani <andolasoft.user179@gmail.com>
2024-06-19 13:49:34 +0000

    Add approve reject buttons on timesheet page

--- src/views/ApprovalRequests.vue
+++ src/views/ApprovalRequests.vue
@@ -38,7 +38,7 @@
                     <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                         <img v-if="val.status == 'Pending'" src="/src/assets/images/pending.svg" alt="" class="h-5 w-5">
                         <img v-else-if="val.status == 'Reject'" src="/src/assets/images/ban.svg" alt="" class="h-5 w-5">
-                        <img v-else src="/src/assets/images/circle-check.svg" alt="" class="h-5 w-5">
+                        <img v-else src="/src/assets/images/circle-check-thin.svg" alt="" class="h-5 w-5">
                     </td>
                     <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
                         <!-- <a href="#" class="text-indigo-600 hover:text-indigo-900"
